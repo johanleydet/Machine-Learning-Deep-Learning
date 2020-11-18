99676e0f8bf911305a4f12c2c554baa6bd741c69
johanleydet <74605184+johanleydet@users.noreply.github.com>
2020-11-18 10:51:40 +0100

    Add files via upload

---
+++ Gradient Descent/Gradcheck.py
@@ -0,0 +1,17 @@
+import torch
+from classes import mse, linear
+
+# Test du gradient de MSE
+
+yhat = torch.randn(10,5, requires_grad=True, dtype=torch.float64)
+y = torch.randn(10,5, requires_grad=True, dtype=torch.float64)
+print(torch.autograd.gradcheck(mse, (yhat, y)))
+
+# Test du gradient de Linear
+
+X=torch.randn(10,20,requires_grad=True, dtype=torch.float64)
+W=torch.randn(20,5,requires_grad=True, dtype=torch.float64)
+b=torch.randn(5,requires_grad=True, dtype=torch.float64)
+
+print(torch.autograd.gradcheck(linear, (X,W,b)))
+
